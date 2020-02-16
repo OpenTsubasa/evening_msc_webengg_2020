c3480af0ae446031446b380c72455c33c46ffeb4
Md. Tanvir Hossain <mth.cse.ru@gmail.com>
2020-02-16 10:03:58 +0600

    Laravel Demo Project Added

---
+++ blog/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Possimus expedita ad fugiat quibusdam? Delectus nemo perspiciatis, 
+        quidem numquam commodi aut dolor. Dolor, sit sunt omnis ex ad incidunt
+         repellendus quo.
+    </div>
+</body>
+</html>
